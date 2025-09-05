e0e3348e5a4b20f9c407d66a4a389b991ec99c7a
Henrik Böving <hargonix@gmail.com>
2025-09-05 17:33:13 +0200

    wip monomorphiser

--- Nunchaku/Transformation/Monomorphization.lean
+++ Nunchaku/Transformation/Monomorphization.lean
@@ -1,5 +1,6 @@
 import Nunchaku.Util.Pipeline
 import Nunchaku.Util.Model
+import Std.Data.Iterators
 
 /-!
 This module contains the monomorphisation transformation, it is based on
@@ -31,19 +32,19 @@ private inductive FlowTypeArg where
   /--
   A (potentially polymorphic) type with arguments.
   -/
-  | const (name : Name) (args : List FlowTypeArg)
+  | const (name : Name) (us : List Level) (args : Array FlowTypeArg)
   deriving Inhabited, BEq, Hashable
 
 private def FlowTypeArg.findTypeVar (type : FlowTypeArg) : Option FlowVariable :=
   match type with
   | .index var .. => some var
-  | .const _ args => Id.run do
+  | .const _ _ args => Id.run do
     for arg in args do
       if let some var := findTypeVar arg then
         return some var
     return none
 
-private def FlowTypeArg.findTypeVarIn (types : List FlowTypeArg) : Option FlowVariable := Id.run do
+private def FlowTypeArg.findTypeVarIn (types : Array FlowTypeArg) : Option FlowVariable := Id.run do
   for type in types do
     if let some var := findTypeVar type then
       return some var
@@ -55,7 +56,7 @@ where
   go (ty : FlowTypeArg) : MessageData :=
     match ty with
     | .index var idx => m!"{var}_({idx})"
-    | .const name args => m!"{name} {args.map go}"
+    | .const name us args => m!"{name}.{us} {args.map go}"
 
 /--
 The inputs into a flow variable.
@@ -68,7 +69,7 @@ private inductive FlowInput where
   /--
   If we want to state what flow into individual components of our flow variable.
   -/
-  | vec (vec : List FlowTypeArg)
+  | vec (vec : Array FlowTypeArg)
   deriving Inhabited, BEq, Hashable
 
 instance : ToMessageData FlowInput where
@@ -96,7 +97,7 @@ private inductive GroundTypeArg where
   /--
   A list of ground type arguments applied to a constant are ground.
   -/
-  | const (name : Name) (args : List GroundTypeArg)
+  | const (name : Name) (us : List Level) (args : Array GroundTypeArg)
   deriving Inhabited, BEq, Hashable
 
 instance : ToMessageData GroundTypeArg where
@@ -104,13 +105,13 @@ instance : ToMessageData GroundTypeArg where
 where
   go (arg : GroundTypeArg) : MessageData :=
     match arg with
-    | .const name args => m!"{toMessageData name} {args.map go}"
+    | .const name us args => m!"{toMessageData name}.{us} {args.map go}"
 
 /--
 An assignment to a vector of type variables.
 -/
 private structure GroundInput where
-  args : List GroundTypeArg
+  args : Array GroundTypeArg
   deriving Inhabited, BEq, Hashable
 
 instance : ToMessageData GroundInput where
@@ -126,7 +127,7 @@ private structure GroundConstraint where
 
 private def FlowTypeArg.toGroundTypeArg (type : FlowTypeArg) : Option GroundTypeArg := do
   match type with
-  | .const name args => return .const name (← args.mapM FlowTypeArg.toGroundTypeArg)
+  | .const name us args => return .const name us (← args.mapM FlowTypeArg.toGroundTypeArg)
   | .index .. => none
 
 private def FlowInput.toGroundInput (inp : FlowInput) : Option GroundInput := do
@@ -137,7 +138,11 @@ private def FlowInput.toGroundInput (inp : FlowInput) : Option GroundInput := do
 
 private def GroundTypeArg.toFlowTypeArg (arg : GroundTypeArg) : FlowTypeArg :=
   match arg with
-  | .const name args => .const name (args.map GroundTypeArg.toFlowTypeArg)
+  | .const name us args => .const name us (args.map GroundTypeArg.toFlowTypeArg)
+
+private partial def GroundTypeArg.toExpr (arg : GroundTypeArg) : Expr :=
+  match arg with
+  | .const name us args => mkAppN (.const name us) (args.map GroundTypeArg.toExpr)
 
 structure MonoAnalysisState where
   /--
@@ -166,26 +171,26 @@ private def getMonoArgPositions (const : Name) : MonoAnalysisM (Array Nat) := do
   Meta.forallTelescope ty fun args _ => do
     let mut positions := #[]
     for h : idx in 0...args.size do
-      if ← Meta.isTypeFormer args[idx] then
+      if ← Meta.isType args[idx] then
         positions := positions.push idx
 
     modifyThe MonoAnalysisState fun s => { s with argPos := s.argPos.insert const positions }
 
     return positions
 
-private def FlowInput.ofTypes (types : List FlowTypeArg) : MonoAnalysisM FlowInput := do
+private def FlowInput.ofTypes (types : Array FlowTypeArg) : MonoAnalysisM FlowInput := do
   let firstType := types[0]!
   match firstType with
   | .index flowVar 0 =>
-    let mut shouldIdx := 1
-    for elem in types.tail do
+    for shouldIdx in 1...types.size do
+      let elem := types[shouldIdx]!
       match elem with
       | .index flowVar' isIdx =>
         if flowVar != flowVar' || isIdx != shouldIdx then
           return .vec types
       | _ => return .vec types
-      shouldIdx := shouldIdx + 1
-    if shouldIdx == (← getMonoArgPositions flowVar.function).size then
+
+    if types.size == (← getMonoArgPositions flowVar.function).size then
       return .var flowVar
     else
       return .vec types
@@ -242,32 +247,31 @@ where
     modify fun s => { s with constraints := s.constraints.insert constr }
 
   flowTypeOfExpr (expr : Expr) : CollectM FlowTypeArg := do
-    let expr ← Meta.whnfR expr
     match expr with
     | .fvar fvarId =>
       let some flow := (← read).flowFVars.get? fvarId
         | throwError m!"Can't interpret {expr} as a flow type"
       return flow
-    | .const const .. =>
+    | .const const us =>
       let positions ← getMonoArgPositions const
       if !positions.isEmpty then
         throwError m!"Underapplied constant cannot be used as flow type: {expr}"
-      return .const const []
+      return .const const us #[]
     | .app .. =>
       expr.withApp fun fn args => do
-        match fn.constName? with
-        | some fn =>
+        match fn with
+        | .const fn us =>
           if TransforM.isBuiltin fn then
             throwError m!"Can't interpret {expr} as a flow type"
           let monoArgPositions ← getMonoArgPositions fn
           if monoArgPositions.isEmpty then
-            return .const fn []
+            return .const fn us #[]
           let last := monoArgPositions[monoArgPositions.size - 1]!
           if args.size ≤ last then
             throwError m!"Can't interpret {expr} as a flow type"
           let flowTypes ← monoArgPositions.mapM (fun idx => flowTypeOfExpr args[idx]!)
-          return .const fn flowTypes.toList
-        | none => throwError m!"Can't interpret {expr} as a flow type"
+          return .const fn us flowTypes
+        | _ => throwError m!"Can't interpret {expr} as a flow type"
     | .mdata _ e => flowTypeOfExpr e
     | .proj .. | .lit .. | .sort .. | .bvar .. | .mvar .. | .forallE .. | .letE .. | .lam .. =>
       throwError m!"Can't interpret {expr} as a flow type"
@@ -301,10 +305,10 @@ where
             let induct := ctorInfo.induct
             if (← getMonoArgPositions induct).size != monoArgPositions.size then
               throwError m!"Encountered inductive type with type indices or existentials: {expr}"
-            addConstraint ⟨ctorInfo.induct⟩ (← FlowInput.ofTypes flowTypes.toList)
-          | .inductInfo .. | .defnInfo .. | .thmInfo .. | .axiomInfo .. =>
-            addConstraint ⟨fn⟩ (← FlowInput.ofTypes flowTypes.toList)
-          | .recInfo .. | .quotInfo .. | .opaqueInfo ..  =>
+            addConstraint ⟨ctorInfo.induct⟩ (← FlowInput.ofTypes flowTypes)
+          | .inductInfo .. | .defnInfo .. =>
+            addConstraint ⟨fn⟩ (← FlowInput.ofTypes flowTypes)
+          | .recInfo .. | .quotInfo .. | .opaqueInfo .. | .axiomInfo .. | .thmInfo .. =>
             throwError m!"Cannot monomorphise {expr}"
         | none => collectExpr fn
     | .lam .. =>
@@ -395,14 +399,14 @@ where
 
   partiallyInstantiateFlowType (arg : FlowTypeArg) (fact : GroundConstraint) : FlowTypeArg :=
     match arg with
-    | .const name args => .const name (partiallyInstantiate args fact)
+    | .const name us args => .const name us (partiallyInstantiate args fact)
     | .index var idx =>
       if var == fact.dst then
         fact.src.args[idx]! |>.toFlowTypeArg
       else
         .index var idx
 
-  partiallyInstantiate (args : List FlowTypeArg) (fact : GroundConstraint) : List FlowTypeArg :=
+  partiallyInstantiate (args : Array FlowTypeArg) (fact : GroundConstraint) : Array FlowTypeArg :=
     args.map (partiallyInstantiateFlowType · fact)
 
   workRule (rule : FlowConstraint) : SolveM Unit := do
@@ -453,13 +457,237 @@ private def SpecializeM.run (x : SpecializeM α) (ctx : SpecializeContext) :
     TransforM (α × SpecializeState) :=
   StateRefT'.run (ReaderT.run x ctx) {}
 
-private def generateSpecialisations : SpecializeM Unit := sorry
+private partial def specialize (g : MVarId) : SpecializeM MVarId := do
+  for (const, inputs) in (← read).solution do
+    for input in inputs do
+      let name ← mkAuxDeclName const.function
+      modify fun s => { s with specialisationCache := s.specialisationCache.insert (const, input) name }
 
-private def applySpecialisations (g : MVarId) : SpecializeM MVarId := sorry
+  for (const, inputs) in (← read).solution do
+    for input in inputs do
+      specialiseConst const.function input
 
-private def specialize (g : MVarId) : SpecializeM MVarId := do
-  generateSpecialisations
-  applySpecialisations g
+  for (const, eqs) in (← TransforM.getEquations) do
+    if !(← read).solution.contains ⟨const⟩ then
+      trace[nunchaku.mono] m!"Skipping specialisation of {const}"
+      modify fun s => { s with newEquations := s.newEquations.insert const eqs }
+
+  -- TODO: traverse the goal
+  return g
+where
+  getMonoArgPositions (const : Name) : SpecializeM (Array Nat) := do
+    return (← read).analysis.argPos[const]!
+
+  partitionMonoArgPositions (const : Name) (args : Array Expr) :
+      SpecializeM (Array Expr × Array (Expr × Nat)) := do
+    let positions ← getMonoArgPositions const
+    let mut others := #[]
+    let mut targets := #[]
+    for idx in 0...args.size do
+      if let some posIdx := positions.findIdx? (· == idx) then
+        targets := targets.push (args[idx]!, posIdx)
+      else
+        others := others.push args[idx]!
+
+    return (others, targets)
+
+  groundTypeOfExpr (expr : Expr) : SpecializeM GroundTypeArg := do
+    match expr with
+    | .const const us =>
+      let positions ← getMonoArgPositions const
+      if !positions.isEmpty then
+        throwError m!"Underapplied constant cannot be used as ground type: {expr}"
+      return .const const us #[]
+    | .app .. =>
+      expr.withApp fun fn args => do
+        match fn with
+        | .const fn us =>
+          if TransforM.isBuiltin fn then
+            throwError m!"Can't interpret {expr} as a ground type"
+          let monoArgPositions ← getMonoArgPositions fn
+          if monoArgPositions.isEmpty then
+            return .const fn us #[]
+          let groundTypes ← monoArgPositions.mapM (fun idx => groundTypeOfExpr args[idx]!)
+          return .const fn us groundTypes
+        | _ => throwError m!"Can't interpret {expr} as a ground type"
+    | .mdata _ e => groundTypeOfExpr e
+    | .proj .. | .lit .. | .sort .. | .bvar .. | .mvar .. | .forallE .. | .letE .. | .lam ..
+    | .fvar .. =>
+      throwError m!"Can't interpret {expr} as a ground type"
+
+  specialiseExpr (expr : Expr) : SpecializeM Expr := do
+    match expr with
+    | .app .. =>
+      expr.withApp fun fn args => do
+        match fn with
+        | .const fn us =>
+          if TransforM.isBuiltin fn then
+            let args ← args.mapM specialiseExpr
+            return mkAppN (.const fn us) args
+          else
+            match ← getConstInfo fn with
+            | .ctorInfo info =>
+              let (others, targets) ← partitionMonoArgPositions info.name args
+              let pattern : FlowVariable × GroundInput :=
+                ⟨⟨info.induct⟩, ⟨← targets.mapM (fun (e, _) => groundTypeOfExpr e)⟩⟩
+              let specialisedInductName := (← get).specialisationCache[pattern]!
+              let remainingArgs ← others.mapM specialiseExpr
+              let specialisedName ← specialisedCtorName specialisedInductName info.name
+              -- TODO: universe levels
+              return mkAppN (.const specialisedName []) remainingArgs
+            | .inductInfo info | .defnInfo info =>
+              let (others, targets) ← partitionMonoArgPositions info.name args
+              let pattern : FlowVariable × GroundInput :=
+                ⟨⟨info.name⟩, ⟨← targets.mapM (fun (e, _) => groundTypeOfExpr e)⟩⟩
+              let specialisedName := (← get).specialisationCache[pattern]!
+              let remainingArgs ← others.mapM specialiseExpr
+              -- TODO: universe levels
+              return mkAppN (.const specialisedName []) remainingArgs
+            | .recInfo .. | .quotInfo .. | .opaqueInfo .. | .axiomInfo .. | .thmInfo .. =>
+              throwError m!"Cannot monomorphise {expr}"
+        | _ =>
+          let args ← args.mapM specialiseExpr
+          return mkAppN (← specialiseExpr fn) args
+    | .lam .. =>
+      Meta.lambdaBoundedTelescope expr 1 fun args body => do
+        let arg := args[0]!
+        let fvarId := arg.fvarId!
+        let name ← fvarId.getUserName
+        let bi ← fvarId.getBinderInfo
+        let newType ← specialiseExpr (← fvarId.getType)
+
+        Meta.withLocalDecl name bi newType fun replacedArg => do
+          let body := body.replaceFVarId fvarId replacedArg
+          Meta.mkLambdaFVars #[replacedArg] (← specialiseExpr body)
+    | .forallE .. =>
+      Meta.forallBoundedTelescope expr (some 1) fun args body => do
+        let arg := args[0]!
+        let fvarId := arg.fvarId!
+        let name ← fvarId.getUserName
+        let bi ← fvarId.getBinderInfo
+        let newType ← specialiseExpr (← fvarId.getType)
+
+        Meta.withLocalDecl name bi newType fun replacedArg => do
+          let body := body.replaceFVarId fvarId replacedArg
+          Meta.mkForallFVars #[replacedArg] (← specialiseExpr body)
+    | .letE (nondep := nondep) .. =>
+      Meta.letBoundedTelescope expr (some 1) fun args body => do
+        let arg := args[0]!
+        let fvarId := arg.fvarId!
+        let name ← fvarId.getUserName
+        let newType ← specialiseExpr (← fvarId.getType)
+        let newValue ← specialiseExpr (← fvarId.getValue?).get!
+
+        Meta.withLetDecl name newType newValue (nondep := nondep) fun replacedArg => do
+          let body := body.replaceFVarId fvarId replacedArg
+          Meta.mkLetFVars #[replacedArg] (← specialiseExpr body)
+    | .mdata _ e => specialiseExpr e
+    | .proj _ _ _ => throwError m!"Don't know how to specialise projection {expr}"
+    | .const .. | .lit .. | .sort .. | .fvar .. | .bvar .. | .mvar .. => return expr
+
+  specialiseConstType (info : ConstantVal) (input : GroundInput) : SpecializeM Expr := do
+    Meta.forallTelescope info.type fun args out => do
+      let name := info.name
+      let (others, targets) ← partitionMonoArgPositions name args
+      let others := others.map Expr.fvarId!
+      let subst : Meta.FVarSubst := targets.foldl (init := {})
+        fun acc (arg, posIdx) => acc.insert arg.fvarId! input.args[posIdx]!.toExpr
+
+      let replacedOthers ← others.mapM fun other => do
+        let currType ← other.getType
+        let argFreeType := currType.applyFVarSubst subst
+        return (← other.getUserName, ← specialiseExpr argFreeType)
+
+
+      Meta.withLocalDeclsDND replacedOthers fun replacedOthers => do
+        let mut subst := subst
+        for (orig, replace) in others.iter.zip replacedOthers.iter do
+          subst := subst.insert orig replace
+        let out := out.applyFVarSubst subst
+        let out ← specialiseExpr out
+        Meta.mkForallFVars replacedOthers out
+
+  specialiseConst (name : Name) (input : GroundInput) : SpecializeM Unit := do
+    trace[nunchaku.mono] m!"Specialising {name} for {input}"
+
+    let info ← getConstInfo name
+    match info with
+    | .inductInfo info => specialiseInduct info input
+    | .defnInfo info => specialiseDefn info input
+    | _ => unreachable!
+
+  specialiseInduct (info : InductiveVal) (input : GroundInput) : SpecializeM Unit := do
+    -- TODO: mutual specialisation
+    if info.all.length != 1 then
+      throwError m!"Can't monomorphise mutual inductives: {info.all}"
+
+    let name := info.name
+    let specName := (← get).specialisationCache[(FlowVariable.mk name, input)]!
+    let specType ← specialiseConstType info.toConstantVal input
+    let newCtors ← info.ctors.mapM (specialiseCtor specName · input)
+
+    --let decl := {
+    --  name := specName,
+    --  type := specType,
+    --  ctors := newCtors
+    --}
+    ---- TODO
+    --let levels := []
+    --let nparams := info.numParams - (← getMonoArgPositions name).size
+    trace[nunchaku.mono] m!"Proposing {specType} and {newCtors.map (·.type)}"
+    --addDecl <| .inductDecl levels nparams [decl] false
+
+  specialisedCtorName (inductSpecName : Name) (ctorName : Name) : SpecializeM Name := do
+    let .str _ n := ctorName | throwError m!"Weird ctor name {ctorName}"
+    return .str inductSpecName n
+
+  specialiseCtor (inductSpecName : Name) (ctorName : Name) (input : GroundInput) :
+      SpecializeM Constructor := do
+    let info ← getConstVal ctorName
+    let specName ← specialisedCtorName inductSpecName ctorName
+    let specType ← specialiseConstType info input
+    return ⟨specName, specType⟩
+
+
+  specialiseDefn (info : DefinitionVal) (input : GroundInput) : SpecializeM Unit := do
+    let name := info.name
+    trace[nunchaku.mono] m!"Specialising def {name}"
+    let specName := (← get).specialisationCache[(FlowVariable.mk name, input)]!
+    let specType ← specialiseConstType info.toConstantVal input
+    trace[nunchaku.mono] m!"Proposing {specType}"
+
+    --let defn := {
+    --  name := specName,
+    --  -- TODO
+    --  levelParams := [],
+    --  type := specType,
+    --  value := ← Meta.mkSorry specType false,
+    --  hints := .opaque,
+    --  safety := .safe
+    --}
+    --addDecl <| .defnDecl defn
+
+
+    let equations ← TransforM.getEquationsFor name
+    let newEqs ← equations.mapM (specialiseEquation name · input)
+    modify fun s => { s with newEquations := s.newEquations.insert specName newEqs }
+
+  specialiseEquation (name : Name) (eq : Expr) (input : GroundInput) :
+      SpecializeM Expr :=
+    /-
+    Equation is of form
+    ∀ ..., f args = body
+
+    1. Telescope over args
+    2. fetch specialisation positions
+    3. based on that identify relevant fvars by inspecting `args`
+    4. build substitution from fvars to input and apply
+    5. replace all types, constructor calls and recursive function calls recursively with their
+       specialised equivalents
+    6. re-abstract over non specialised fvars
+    -/
+    -- TODO
+    return eq
 
 def transformation : Transformation MVarId MVarId LeanResult LeanResult where
   st := Unit
@@ -473,8 +701,8 @@ def transformation : Transformation MVarId MVarId LeanResult LeanResult where
         trace[nunchaku.mono] m!"Constraints: {constraints}"
         let solution := solveConstraints constraints (by simpa using h)
         trace[nunchaku.mono] m!"Solution: {solution.toList}"
-        --let (g, st) ← (specialize g).run { analysis := monoAnalysis, solution }
-        --TransforM.replaceEquations st.newEquations
+        let (g, st) ← (specialize g).run { analysis := monoAnalysis, solution }
+        TransforM.replaceEquations st.newEquations
         return (g, ())
     decode _ res := return res
   }
--- Test/Mono.lean
+++ Test/Mono.lean
@@ -1,5 +1,26 @@
 import Nunchaku
 
+namespace Foo
+
+inductive MList (α : Type) where
+  | nil
+  | cons (x : α) (xs : MList α)
+
+def MList.map (xs : MList α) (f : α → β) : MList β :=
+  match xs with
+  | .nil => .nil
+  | .cons x xs => .cons (f x) (map xs f)
+
+set_option trace.nunchaku true in
+example  : MList.nil.map Nat.succ ≠ (.nil : MList Nat) := by
+  nunchaku
+
+end Foo
+
+set_option trace.nunchaku true in
+example  : [].map id ≠ ([] : List Nat) := by
+  nunchaku
+
 set_option trace.nunchaku true in
 example (xs : List α) : xs.map id ≠ xs := by
   nunchaku
@@ -12,7 +33,6 @@ set_option trace.nunchaku true in
 example (xs : List (List α)) : xs.map (·.map id) ≠ xs := by
   nunchaku
 
-
 def sumalt : List Nat → Nat :=
   List.foldr (· + ·) .zero
 
@@ -22,4 +42,12 @@ example (xs : List Nat) (h : xs ≠ []) : sumalt xs ≠ .zero := by
 
 set_option trace.nunchaku true in
 example (xs : List Nat) (h : xs ≠ []) : xs.sum ≠ .zero := by
+  nunchaku
+
+set_option trace.nunchaku true in
+example (xs : List (Fin n)) : xs.map id ≠ xs := by
+  nunchaku
+
+set_option trace.nunchaku true in
+example (xs : List (Fin .zero)) : ¬ xs.isEmpty := by
   nunchaku
